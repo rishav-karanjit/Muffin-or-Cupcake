94f7997122859afd4602a48c4756f0b3aa0b5b53
unknown <karanjitrishav4@gmail.com>
2020-09-23 22:10:03 +0545

    Program completed

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ from sklearn import svm
 import matplotlib.pyplot as plt
 import seaborn as sns; sns.set(font_scale=1.2)
 
-recipes = pd.read_csv("Muffin or Cupcake.csv")
+recipes = pd.read_csv("recipes_muffins_cupcakes.csv")
 
-sns.lmplot('Flour','Sugar',data=recipes, hue='Type', palette='Set1',fit_reg=False,scatter_kws={"s":70})
+# sns.lmplot(x= 'Flour',y= 'Sugar',data=recipes, hue='Type', palette='Set1',fit_reg=False,scatter_kws={"s":70})
 # plt.show()
 
 type_label = np.where(recipes['Type']=='Muffin',0,1)
@@ -18,3 +18,28 @@ ingredients = recipes[['Flour','Sugar']].values
 
 model = svm.SVC(kernel='linear')
 model.fit(ingredients, type_label)
+
+w = model.coef_[0]
+a = -w[0] / w[1]
+xx = np.linspace(30,60)
+yy = a * xx - (model.intercept_[0] / w[1])
+
+b = model.support_vectors_[0]
+yy_down = a * xx + (b[1]- a * b[0])
+b = model.support_vectors_[-1]
+yy_up = a * xx +(b[1] - a * b[0])
+
+sns.lmplot(x='Flour',y='Sugar',data=recipes, hue='Type', palette='Set1',fit_reg=False,scatter_kws={"s":70})
+# plt.plot(xx , yy, linewidth=2, color='black')
+# plt.plot(xx , yy_down, 'k--')
+# plt.plot(xx , yy_up, 'k--')
+
+# plt.show()
+
+F = int(input("Enter Flour amount:"))
+S = int(input("Enter Sugar amount:"))
+
+if(model.predict([[F,S]]) == 0):
+    print("Its Muffin recipe")
+else:
+    print("Its Cupcake recipe")
